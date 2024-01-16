fabe8ad5ed3757bb74bab23be45b9118bce938d6
Tedox-hub <70034926+Tedox-hub@users.noreply.github.com>
2024-01-16 23:20:32 +0100

    Add files via upload

---
+++ frontend/src/components/DropDownMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-form-select v-model="selected" :options="options"></b-form-select>
+    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+export default {
+  data(): { selected: string | null; options: { value: string | null; text: string; disabled?: boolean }[] } {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: 'Please select some item' },
+        { value: 'a', text: 'Product Owner' },
+        { value: 'b', text: 'This is not an option:' },
+        { value: 'c', text: 'Tester' },
+        { value: 'd', text: 'This one is disabled', disabled: true },
+        { value: 'c', text: 'Dev rawrrr UwU' },
+      ],
+    };
+  },
+};
+
+</script>
---
+++ frontend/src/components/PasswordReset.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Hallo</h1><br>
+    <h2>Dein bisheriges Passwort ist</h2><br><br>
+    <button id="resetPW">Passwort zurücksetzen</button>
+    <button id="backToLogin">Zurück zum Login</button><br>
+    <label for="fastLogin">DANGER:</label>
+    <button id="fastLogin">Admin-Schnell-Login</button>
+</template>
